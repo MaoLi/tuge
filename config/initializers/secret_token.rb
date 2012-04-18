ac1d4014650357b645f05069c2f046d068961ed5
stcatz <stcatzlee@gmail.com>
2012-04-18 15:15:59 +0800

    initial repository.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuge::Application.config.secret_token = 'a2cbd0eebc2f1709e52c0f2181494ba34bf23c676f84efbd98f68709e87abb25ae9292de5eb7f43c39b28de9e42b91fa15fc23294fa117b7d9150672ba9fe539'
